3db8a40fa59e7e6c3331165b08c0b15ed2f5e7fa
Geoff Fox <fox.geoffrey@gmail.com>
2020-06-02 00:58:35 -0700

    Refactor: remove NAvigation Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
-   { path: '', loadChildren: () => import('./navigation/navigation.module').then(m => m.NavigationModule) },
-   { path: 'signup', loadChildren: () => import('./navigation/navigation.module').then(m => m.NavigationModule) },
    { path: 'welcome', component: WelcomeComponent },
-   // { path: 'signup', component: SignupComponent },
+   { path: 'signup', component: SignupComponent },
    { path: 'login', component: LoginComponent },
-  ];
 
+   { path: '', component: WelcomeComponent, pathMatch: 'full' }, // redirect
+   // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+
+  ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,40 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { NavigationModule } from './navigation/navigation.module';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
+import { SidenavComponent } from './navigation/sidenav/sidenav.component';
+import { CommonModule } from '@angular/common';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { AuthService } from './auth/auth.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+
+    SidenavComponent,
+    SidenavListComponent,
+    HeaderComponent,
+
     WelcomeComponent,
-    //SignupComponent,
+    SignupComponent,
     LoginComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    NavigationModule,
     FormsModule,
     ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
