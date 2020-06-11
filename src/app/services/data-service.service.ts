2fdf063b14146867047a550817aad12e788370a5
Geoff Fox <fox.geoffrey@gmail.com>
2020-06-10 22:45:57 -0700

    Add: dataService

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book';
+import { BookTrackerError } from '../models/bookTrakerError';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,12 @@ import { Injectable } from '@angular/core';
 export class DataServiceService {
 
   constructor() { }
+
+  getAllBooks(): Observable<Book>[] | BookTrackerError {
+    console.log('Getting all books from the server.');
+    return this.http.get<Book[]>('/api/books')
+      .pipe(
+        catchError(err => this.handleHttpError(err))
+      );
+  };
 }
